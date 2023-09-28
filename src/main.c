0bad468793027e20598dee390f958cfae02010fe
Replit user <>
2023-09-28 13:05:51 +0000

    Add mg_arena.h and mg_plot.h

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+#include "mg/mg_arena.h"
+#include "mg/mg_plot.h"
+
+void mga_on_error(mga_error err) {
+    fprintf(stderr, "MGA Error %u: %s\n", err.code, err.msg);
+}
+
 int main(void) {
-    printf("Hello World\n");
+    mga_desc desc = {
+        .desired_max_size = MGA_MiB(64),
+        .desired_block_size = MGA_KiB(256),
+        .error_callback = mga_on_error
+    };
+    mg_arena* perm_arena = mga_create(&desc);
+
+    mga_destroy(perm_arena);
 
     return 0;
 }
---
+++ src/third_party/mg/mg_impl.c
@@ -0,0 +1,7 @@
+#include "mg_arena.h"
+
+#define MG_ARENA_IMPL
+#include "mg_arena.h"
+
+#define MG_PLOT_IMPL
+#include "mg_plot.h"
